0b532bfcb550879c9c855e2d1dd280340306ea83
Kuczynior <maksymkuczynski@gmail.com>
2023-04-15 21:01:43 +0200

    Validate password and Element Count

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,15 @@
 class CountElements
+  def initialize(elements)
+    @elements = elements
+  end
 
+  def perform
+    elements_sum = {}
+    @elements.each do |element|
+      elements_sum[element] ||= 0
+      elements_sum[element] = elements_sum[element] + 1
+    end
+    elements_sum
+  end
 end
+
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,22 @@
 class ValidatePassword
+  def initialize(password)
+    @password  = password
+  end
+
+  def perform
+    return false if @password.length < 6
+    return false if @password.length > 24
+    return false if (@password.split("") &  ("A".."Z").to_a).empty?
+    return false if (@password.split("") &  ("a".."z").to_a).empty?
+    return false if (@password.split("") &  ("0".."9").to_a).empty?
+
+    return false if (@password.split("") &  ["!","@", "#", "$", "%", "&," "*", "+", "=", ":", ";", "?", "<", ">"]).empty?
+
+    3.upto(@password.length) do |index|
+      return false if @password[index-1] == @password[index-2] && @password[index-1] == @password[index]
+    end
+
+    true
+  end
 
 end
